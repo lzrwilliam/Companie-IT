46992818935cef5e4eeeeb0794fd5bc8b1425bfa
lazar <william-patrick.lazar@s.unibuc.ro>
2022-12-01 17:13:37 +0200

    .

--- main.cpp
+++ main.cpp
@@ -17,12 +17,8 @@ int main() {
 //    cout<<c1;
 
     NetworkEngineer n("William", 100, 1, 1, 3, 4);
-//cout<<n;
     OperatorCallCenter n1("William12", 0, 1, 1, 1, 1);
-
     DepartamenteTehnice d1("Retele", {n.clone(), n1.clone()}, 100, 3);
-    //cout << d1;
-    DepartamenteTehnice d2("Retele", {n.clone()}, 100, 3);
     DepartamentRelatiiClienti d3("Call Center", {n1.clone()}, 5);
 
     cout << d3;
